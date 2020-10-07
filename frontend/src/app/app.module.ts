1cb059a0d24dd67c58f331f28fb486003517348f
Rafaela Lima Nogueira <rn-15@hotmail.com>
2020-10-07 14:58:15 -0300

    imports

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,19 +10,26 @@ import { FooterComponent } from './components/template/footer/footer.component';
 
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { NavComponent } from './components/template/nav/nav.component';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
