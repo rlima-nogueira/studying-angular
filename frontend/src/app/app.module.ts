22e2ea5d991757ff82b25dbf476afb437c1b5e45
Rafaela Lima Nogueira <rn-15@hotmail.com>
2020-10-08 10:39:01 -0300

    component nav

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,11 +7,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HeaderComponent } from './components/template/header/header.component';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
+
 
 
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { NavComponent } from './components/template/nav/nav.component';
-
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 
