827bc3c33f501ee936ac277a51313bbff5adaf03
Rafaela Lima Nogueira <rn-15@hotmail.com>
2020-10-09 16:57:11 -0300

    product update

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -33,6 +33,7 @@ import { ProductReadComponent } from './components/product/product-read/product-
 
 import localePt from '@angular/common/locales/pt';
 import {registerLocaleData} from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 
 registerLocaleData(localePt);
@@ -45,7 +46,8 @@ registerLocaleData(localePt);
     HomeComponent,
     ProductCrudComponent,
     ProductCreateComponent,
-    ProductReadComponent
+    ProductReadComponent,
+    ProductUpdateComponent
   ],
   imports: [
     BrowserModule,
