ac7aa85e7a0f8449829070e3d5f6954f61e314fd
Rafaela Lima Nogueira <rn-15@hotmail.com>
2020-10-09 08:56:04 -0300

    show message with snackbar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -17,7 +17,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatSnackBarModule} from '@angular/material/snack-bar';
 
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
@@ -41,7 +41,8 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     MatSidenavModule,
     MatListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
