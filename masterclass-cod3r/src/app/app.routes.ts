fdf7336e00f62e8ef96c109cad912d9d4a146de9
Rafaela Lima <rn-15@hotmail.com>
2026-05-12 15:40:00 -0300

    configurando projeto

--- masterclass-cod3r/src/app/app.routes.ts
+++ masterclass-cod3r/src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { Home } from './screens/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  }
+];
---
+++ masterclass-cod3r/src/app/components/shared/page/page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Menu } from '../menu/menu';
+import { RouterOutlet } from "@angular/router";
+
+@Component({
+  selector: 'app-page',
+  imports: [Menu, RouterOutlet],
+  templateUrl: './page.html',
+  styleUrl: './page.css',
+})
+export class Page {}
