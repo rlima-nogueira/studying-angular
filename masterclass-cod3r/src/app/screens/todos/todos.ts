f1af165f4470bd36cb6a2c176291f5dc9e713c66
Rafaela Lima <rn-15@hotmail.com>
2026-05-13 11:41:07 -0300

    add keyup

--- masterclass-cod3r/src/app/screens/todos/todos.ts
+++ masterclass-cod3r/src/app/screens/todos/todos.ts
@@ -1,14 +1,26 @@
 import { Component, inject } from '@angular/core';
 import { Title } from '../../components/shared/title/title';
 import { TodosService } from '../../services/todos.service';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-todos',
-  imports: [Title],
+  imports: [Title, MatIconModule],
   templateUrl: './todos.html',
   styleUrl: './todos.css',
 })
 export class Todos {
   readonly todos = inject(TodosService);
+
+  addTodo(event: Event) {
+    event.preventDefault();
+    const input = event.target as HTMLInputElement;
+    const title = input.value.trim();
+    if (title) return;
+
+
+    this.todos.add(title);
+    input.value = '';
+  }
 }
--- masterclass-cod3r/src/app/services/todos.service.ts
+++ masterclass-cod3r/src/app/services/todos.service.ts
@@ -6,10 +6,31 @@ import { Todo } from '../model/todo.entity';
 })
 export class TodosService {
   private readonly _items = signal<Array<Todo>>([
-    { id: crypto.randomUUID(), title: 'Comprar leite', done: false },
-    { id: crypto.randomUUID(), title: 'Comprar leite', done: true },
-    { id: crypto.randomUUID(), title: 'Comprar leite', done: false },
+    { id: crypto.randomUUID(), title: 'Comprar xyz', done: false },
+    { id: crypto.randomUUID(), title: 'Comprar 123', done: true },
+    { id: crypto.randomUUID(), title: 'Comprar 33', done: false },
   ]);
 
   readonly items = this._items.asReadonly();
+
+  toggle(id: string) {
+    this._items.update((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, done: !item.done } : item
+      )
+    );
+  }
+
+  add(title: string) {
+    const newItem: Todo = {
+      id: crypto.randomUUID(),
+      title,
+      done: false,
+    };
+    this._items.update((items) => [...items, newItem]);
+  }
+
+  remove(id: string) {
+    this._items.update((items) => items.filter((item) => item.id !== id));
+  }
 }
